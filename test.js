410cd2d1037d8b6de8cc3e30678ac615553118e4
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-23 14:17:10 -0400

    tweak

--- test.js
+++ test.js
@@ -1,88 +1,88 @@
 'use strict'
 
 import test from 'ava'
-import isGitRepository from './'
+import isGithubRepo from './'
 
 const https = 'https://github.com/bukinoshita/is-git-repository.git'
 const ssh = 'git@github.com:bukinoshita/is-git-repository.git'
 const repo = 'bukinoshita/is-git-repository'
 
 test('https', t => {
-  t.true(isGitRepository(https))
+  t.true(isGithubRepo(https))
 })
 
 test('ssh', t => {
-  t.true(isGitRepository(ssh))
+  t.true(isGithubRepo(ssh))
 })
 
 test('repo', t => {
-  t.true(isGitRepository(repo))
+  t.true(isGithubRepo(repo))
 })
 
 test('fails', t => {
-  const result = isGitRepository('aefwefew')
+  const result = isGithubRepo('aefwefew')
   t.false(result)
 })
 
 test('fails repo without repo', t => {
-  const result = isGitRepository('aefwefew/')
+  const result = isGithubRepo('aefwefew/')
   t.false(result)
 })
 
 test('fails repo without owner', t => {
-  const result = isGitRepository('/aefwefew')
+  const result = isGithubRepo('/aefwefew')
   t.false(result)
 })
 
 test('fails https', t => {
-  const result = isGitRepository('https://github.com/bukinoshita')
+  const result = isGithubRepo('https://github.com/bukinoshita')
   t.false(result)
 })
 
 test('fails ssh', t => {
-  const result = isGitRepository('git@github.com:bukinoshita/')
+  const result = isGithubRepo('git@github.com:bukinoshita/')
   t.false(result)
 })
 
 test('https with type', t => {
-  const result = isGitRepository(https, { withType: true })
-  t.true(result.isGitRepository)
+  const result = isGithubRepo(https, { withType: true })
+  t.true(result.isGithubRepo)
   t.is(result.type, 'https')
 })
 
 test('ssh with type', t => {
-  const result = isGitRepository(ssh, { withType: true })
-  t.true(result.isGitRepository)
+  const result = isGithubRepo(ssh, { withType: true })
+  t.true(result.isGithubRepo)
   t.is(result.type, 'ssh')
 })
 
 test('repo with type', t => {
-  const result = isGitRepository(repo, { withType: true })
-  t.true(result.isGitRepository)
+  const result = isGithubRepo(repo, { withType: true })
+  t.true(result.isGithubRepo)
   t.is(result.type, 'repo')
 })
 
 test('fails with type', t => {
-  const result = isGitRepository('aefwefew', { withType: true })
+  const result = isGithubRepo('aefwefew', { withType: true })
   t.false(result)
 })
 
 test('fails repo without repo with type', t => {
-  const result = isGitRepository('aefwefew/', { withType: true })
+  const result = isGithubRepo('aefwefew/', { withType: true })
   t.false(result)
 })
 
 test('fails repo without owner with type', t => {
-  const result = isGitRepository('/aefwefew', { withType: true })
+  const result = isGithubRepo('/aefwefew', { withType: true })
   t.false(result)
 })
 
 test('fails https with type', t => {
-  const result = isGitRepository('https://github.com/bukinoshita', { withType: true })
+  const result = isGithubRepo('https://github.com/bukinoshita', { withType: true })
   t.false(result)
 })
 
 test('fails ssh with type', t => {
-  const result = isGitRepository('git@github.com:bukinoshita/', { withType: true })
+  const result = isGithubRepo('git@github.com:bukinoshita/', { withType: true })
   t.false(result)
 })
